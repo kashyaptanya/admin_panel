c077dde22a1a3f8670c2022c223d42a866a9ecc9
kashyaptanya <tanyakashayp032@gmail.com>
2023-01-10 16:29:48 -0500

    update

--- src/componenets/signup.js
+++ src/componenets/signup.js
@@ -33,59 +33,68 @@ function Signup() {
             {popup ?
                 <div className="toast show ">
                     <div className="toast-body toast_style">
-                        sign up successfully
+                        Login Successfully
                     </div>
                 </div> : null
             }
             <div className="bg_color">
                 <div className="container">
                     <div className="row p-5 ">
-                        <div className="col-md-4 bg p-5 rounded  ">
-                            <h2 className="pt-5"> Welcome back!</h2>
-                            <p>login with your personal info..</p>
-                            <div className='image'>
-                            <img src={Image}></img>
+                        <div className="col-md-4  rounded  ">
+                            <div className=" bg "></div>   
+                            <div className="text">
+                            <h6 className="">"We have been using untitled to kick start every new project.." </h6><br></br>
+                            <span >Lorem John</span><br></br><span><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></span>
+                         
+                            
                             </div>
+                            {/* <div className='image'>
+                            <img src={Image}></img>
+                            </div> */}
                         </div>
                         <div className="col-md-8 bg-white p-5 rounded postion">
                             <h2 className="color text-center">
-                                Create Account
+                                Welcome back
                             </h2>
-                            <i class="fa-brands fa-facebook rotate"></i>
+                            <p className="text-center">Welcome back! please enter your details</p>
+                            {/* <i class="fa-brands fa-facebook rotate"></i>
                             <i class="fa-brands fa-github rotate2"></i>
-                            <i class="fa-brands fa-linkedin rotate3"></i>
+                            <i class="fa-brands fa-linkedin rotate3"></i> */}
                             {/* <p className="rotate">login with your personal info..</p> */}
-                            <form className="p-5" onSubmit={handle_button} >
+                            <form className="p-4 " onSubmit={handle_button} >
                                 
-                                <div className="form-group p-2 text-secondary">
-                                    <input className="form-control "
+                                <div className="form-group form p-2 mb-3 ">
+                                    <input className=" textbox"
                                         required
                                         type="text"
                                         value={user?.name ?? ""}
                                         onChange={(e) => handlevalue(e, 'name')}
-                                        placeholder="Type your Username"
+                                        placeholder=""
                                     />
+                                    <label className="form-label">Name</label>
                                 </div>
-                                <div className="form-group p-2 text-secondary">
+                                <div className="form-group p-2 mb-3  form">
 
-                                    <input className="form-control"
+                                    <input className="textbox"
                                         required
-                                        type="email"
+                                        type="text"
                                         value={user?.email ?? ""}
                                         onChange={(e) => handlevalue(e, 'email')}
-                                        placeholder="Type your Email"
+                                        placeholder=""
                                     />
+                                    <label className="form-label">Email</label>
                                 </div>
-                                <div className="form-group p-2 text-secondary">
-                                    <input className="form-control"
+                                <div className="form-group p-2 mb-3  form">
+                                    <input className="textbox"
                                         required
                                         type="password"
                                         value={user.password}
                                         onChange={(e) => handlevalue(e, 'password')}
-                                        placeholder="Type your Password"
+                                        placeholder=""
                                     />
+                                    <label className="form-label">password</label>
                                 </div>
-                                <div className="pt-5 text-center">
+                                <div className="pt-3 text-center">
                                     <button type="submit"
                                         className="rounded-pill">
                                         Sign up
